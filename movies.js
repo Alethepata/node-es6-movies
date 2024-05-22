13bb00e16ce83f56d870f689739b3dfa5e1e836e
Alethepata <alessia.patane@gmail.com>
2024-05-22 15:16:02 +0200

    add genre

--- movies.js
+++ movies.js
@@ -2,12 +2,14 @@ const movies = [
     {
         "Title": "Avatar",
         "Year": "2009",
+        "Genre": "Action, Adventure, Fantasy",
         "Rating": "7.9",
         "Type": "movie",
     },
     {
         "Title": "Game of Thrones",
         "Year": "2011",
+        "Genre": "Adventure, Drama, Fantasy",
         "Rating": "9.5",
         "Type": "series",
         "Season": "7",
@@ -15,12 +17,14 @@ const movies = [
     {
         "Title": "The Wolf of Wall Street",
         "Year": "2013",
+        "Genre": "Biography, Comedy, Crime",
         "Rating": "8.2",
         "Type": "movie",
     },
     {
         "Title": "Gotham",
         "Year": "2014",
+        "Genre": "Action, Crime, Drama",
         "Rating": "8.0",
         "Type": "series",
         "Season": "3",
@@ -28,9 +32,10 @@ const movies = [
 ];
 
 class Movie {
-    constructor(title, year, rating, type) {
+    constructor(title, year, genre, rating, type) {
         this.title = title;
         this.year = year;
+        this.genre = genre;
         this.rating = rating;
         this.type = type;
     }
@@ -38,7 +43,7 @@ class Movie {
 
 class TvSerie extends Movie { 
     constructor(title, year, rating, type, season) {
-        super(title, year, rating, type);
+        super(title, year, genre, rating, type);
         this.season = season;
     }
 }
