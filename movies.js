8cac4e0646f7d7694b8c408b7b00e7ee58c41565
Alethepata <alessia.patane@gmail.com>
2024-05-22 15:00:15 +0200

    add movies

---
+++ movies.js
@@ -0,0 +1,28 @@
+const movies = [
+    {
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rating": "7.9",
+        "Type": "movie",
+    },
+    {
+        "Title": "Game of Thrones",
+        "Year": "2011",
+        "Rating": "9.5",
+        "Type": "series",
+        "Season" : "7",
+    },
+    {
+        "Title": "The Wolf of Wall Street",
+        "Year": "2013",
+        "Rating": "8.2",
+        "Type": "movie",
+    },
+    {
+        "Title": "Gotham",
+        "Year": "2014",
+        "Rating": "8.0",
+        "Type": "series",
+        "Season" : "3",
+    },
+]
